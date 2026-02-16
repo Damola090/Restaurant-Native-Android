d9608dfd63db5c0ed343b60bf97ad635195f953f
Damola090 <kadaradamola090@gmail.com>
2026-02-16 15:47:09 +0100

    cache data offline using ROOM

--- app/src/main/java/com/example/kadaracompose/Restaurant.kt
+++ app/src/main/java/com/example/kadaracompose/Restaurant.kt
@@ -1,17 +1,25 @@
 package com.example.kadaracompose
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "restaurants")
 data class Restaurant(
+    @PrimaryKey()
+    @ColumnInfo(name = "r_id")
     @SerializedName("r_id")
     val id: Int,
+    @ColumnInfo(name = "r_title")
     @SerializedName("r_title")
     val title: String,
+    @ColumnInfo(name = "r_description")
     @SerializedName("r_description")
     val description: String,
-    var isFavorite: Boolean = false
+    @ColumnInfo(name = "is_favorite")
+    val isFavorite: Boolean = false
 )
-
 
 //
 //data class Restaurant(val id: Int,
